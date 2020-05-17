551e45504ed251a33395143138bede265b429b7e
Arnaud <couchet.arnaud@gmail.com>
2020-05-17 11:36:15 +0200

    Add a dto. Remove bin.

--- Controllers/CovidItemsController.cs
+++ Controllers/CovidItemsController.cs
@@ -1,15 +1,13 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CovidApi.Models;
 
 namespace CovidApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/thisisanawsomenewdotnetcore32api")]
     [ApiController]
     public class CovidItemsController : ControllerBase
     {
@@ -22,14 +20,14 @@ namespace CovidApi.Controllers
 
         // GET: api/CovidItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CovidItem>>> GetCovidItems()
+        public async Task<ActionResult<IEnumerable<CovidItemDTO>>> GetCovidItems()
         {
-            return await _context.CovidItems.ToListAsync();
+            return await _context.CovidItems.Select(item => ItemToDTO(item)).ToListAsync();
         }
 
         // GET: api/CovidItems/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<CovidItem>> GetCovidItem(long id)
+        public async Task<ActionResult<CovidItemDTO>> GetCovidItem(long id)
         {
             var covidItem = await _context.CovidItems.FindAsync(id);
 
@@ -38,19 +36,25 @@ namespace CovidApi.Controllers
                 return NotFound();
             }
 
-            return covidItem;
+            return ItemToDTO(covidItem);
         }
 
         // PUT: api/CovidItems/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCovidItem(long id, CovidItem covidItem)
+        public async Task<IActionResult> UpdateCovidItem(long id, CovidItemDTO covidItemDTO)
         {
-            if (id != covidItem.Id)
+            if (id != covidItemDTO.Id)
             {
                 return BadRequest();
             }
+
+            var covidItem = new CovidItem {
+                Id = covidItemDTO.Id,
+                IsComplete = covidItemDTO.IsComplete,
+                Name= covidItemDTO.Name
+            };
 
             _context.Entry(covidItem).State = EntityState.Modified;
 
@@ -77,17 +81,22 @@ namespace CovidApi.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<CovidItem>> PostCovidItem(CovidItem covidItem)
+        public async Task<ActionResult<CovidItemDTO>> PostCovidItem(CovidItemDTO covidItemDTO)
         {
+            var covidItem = new CovidItem {
+                IsComplete = covidItemDTO.IsComplete,
+                Name= covidItemDTO.Name
+            };
+
             _context.CovidItems.Add(covidItem);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetCovidItem), new { id = covidItem.Id }, covidItem);
+            return CreatedAtAction(nameof(GetCovidItem), new { id = covidItemDTO.Id }, ItemToDTO(covidItem));
         }
 
         // DELETE: api/CovidItems/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<CovidItem>> DeleteCovidItem(long id)
+        public async Task<ActionResult<CovidItemDTO>> DeleteCovidItem(long id)
         {
             var covidItem = await _context.CovidItems.FindAsync(id);
             if (covidItem == null)
@@ -98,12 +107,19 @@ namespace CovidApi.Controllers
             _context.CovidItems.Remove(covidItem);
             await _context.SaveChangesAsync();
 
-            return covidItem;
+            return ItemToDTO(covidItem);
         }
 
         private bool CovidItemExists(long id)
         {
             return _context.CovidItems.Any(e => e.Id == id);
         }
+
+        private static CovidItemDTO ItemToDTO(CovidItem covidItem) => new CovidItemDTO
+        {
+            Id = covidItem.Id,
+            Name = covidItem.Name,
+            IsComplete = covidItem.IsComplete
+        };
     }
 }
